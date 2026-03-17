9f867b8a9b6449bfbc4364bdfa8e2d62d9041b21
auyyangon <info@connect.edu.mm>
2026-03-17 09:35:01 +0630

    Auto-deploy: integrated Google Sheets data and fixed syntax

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 ﻿export default async function handler(req, res) {
   const { email } = req.query;
-  const API_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYED_SCRIPT_ID/exec';
+  const API_URL = process.env.APPS_SCRIPT_URL;
 
   try {
     const url = new URL(API_URL);
@@ -10,9 +10,6 @@
     const data = await response.json();
     
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
